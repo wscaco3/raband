b7f015cb37bab3d9ec904d55bde2b0e0d2643742
wscaco3 <wscaco3@sina.com>
2016-04-07 16:04:17 +0800

    add list fragment

--- app/src/main/java/top/ewind/raband/ui/MainActivity.java
+++ app/src/main/java/top/ewind/raband/ui/MainActivity.java
@@ -9,26 +9,36 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import top.ewind.raband.R;
 import top.ewind.raband.base.BaseActivity;
+import top.ewind.raband.base.BaseFragment;
 import top.ewind.raband.ui.fragment.FavFragment;
+import top.ewind.raband.ui.fragment.ItemFragment;
 import top.ewind.raband.ui.fragment.MainFragment;
+import top.ewind.raband.ui.fragment.dummy.DummyContent;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ContentView(R.layout.activity_main)
 public class MainActivity extends BaseActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, ItemFragment.OnListFragmentInteractionListener {
     @ViewInject(R.id.toolbar)
     private Toolbar toolbar;
     @ViewInject(R.id.drawer_layout)
     private DrawerLayout drawer;
     @ViewInject(R.id.nav_view)
     private NavigationView navView;
+
+    private Map<String,BaseFragment> fragmentMap;
+    private BaseFragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +50,8 @@ public class MainActivity extends BaseActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navView.setNavigationItemSelectedListener(this);
-        setFragment(0);
+        fragmentMap = new HashMap<String,BaseFragment>();
+        setFragment("home");
     }
 
     @Override
@@ -52,17 +63,43 @@ public class MainActivity extends BaseActivity
         }
     }
 
-    private void setFragment(int seq){
+    private void setFragment(String type){
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
         transaction.setCustomAnimations(R.anim.alpha_enter,R.anim.alpha_exit);
-        if(seq == 0){
-            transaction.replace(R.id.main_content, MainFragment.newInstance("ok"));
+        BaseFragment switchFragment = fragmentMap.get(type);
+        if(switchFragment!=null&&currentFragment!=null&&type.equals(currentFragment.getTag())){
+            return;
         }
-        else if (seq==1){
-            transaction.replace(R.id.main_content, FavFragment.newInstance("ok"));
+        boolean isAdd = false;
+        if(switchFragment==null){
+            if(type.equals("home")){
+                switchFragment = MainFragment.newInstance("ok");
+            }
+            else if(type.equals("find")){
+                switchFragment = FavFragment.newInstance("ok");
+            }
+            else if(type.equals("fav")){
+                switchFragment = ItemFragment.newInstance(1);
+            }
+            else if(type.equals("msg")){
+                switchFragment =  ItemFragment.newInstance(4);
+            }
+            else switchFragment =  MainFragment.newInstance("ok");
+            fragmentMap.put(type,switchFragment);
+            isAdd = true;
         }
-        else transaction.replace(R.id.main_content, MainFragment.newInstance("ok"));
+        if(currentFragment==null){
+            transaction.replace(R.id.main_content, switchFragment,type);
+        }
+        else{
+            transaction.hide(currentFragment);
+            if(isAdd){
+                transaction.add(R.id.main_content, switchFragment,type);
+            }
+            else transaction.show(switchFragment);
+        }
+        currentFragment = switchFragment;
         transaction.commit();
     }
 
@@ -86,13 +123,13 @@ public class MainActivity extends BaseActivity
         int id = item.getItemId();
         Intent intent = new Intent();
         if (id == R.id.nav_camera) {
-            setFragment(0);
+            setFragment("home");
         } else if (id == R.id.nav_gallery) {
-            setFragment(1);
+            setFragment("find");
         } else if (id == R.id.nav_slideshow) {
-
+            setFragment("fav");
         } else if (id == R.id.nav_manage) {
-
+            setFragment("msg");
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
@@ -100,5 +137,10 @@ public class MainActivity extends BaseActivity
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Log.d("MainActivity","onListFragmentInteraction OK");
     }
 }
---
+++ app/src/main/java/top/ewind/raband/ui/fragment/ItemFragment.java
@@ -0,0 +1,82 @@
+package top.ewind.raband.ui.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+import top.ewind.raband.R;
+import top.ewind.raband.base.BaseFragment;
+import top.ewind.raband.ui.fragment.dummy.DummyContent;
+import top.ewind.raband.ui.fragment.dummy.DummyContent.DummyItem;
+
+@ContentView(R.layout.fragment_item_list)
+public class ItemFragment extends BaseFragment {
+
+    @ViewInject(R.id.item_list)
+    private RecyclerView recyclerView;
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+    private OnListFragmentInteractionListener mListener;
+
+
+    public static ItemFragment newInstance(int columnCount) {
+        ItemFragment fragment = new ItemFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Context context = view.getContext();
+        RecyclerView recyclerView = (RecyclerView) view;
+        if (mColumnCount <= 1) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        } else {
+            recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+        }
+        recyclerView.setAdapter(new MyItemRecyclerViewAdapter(DummyContent.ITEMS, mListener));
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnListFragmentInteractionListener) {
+            mListener = (OnListFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnListFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onListFragmentInteraction(DummyItem item);
+    }
+}
